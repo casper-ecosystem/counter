2924f52ccaee94a25820a523e87207eb92a68567
zie1ony <maciek.s.zielinski@gmail.com>
2019-10-14 14:28:01 -0700

    v0.8

--- contracts/counter-call/src/lib.rs
+++ contracts/counter-call/src/lib.rs
@@ -1,29 +1,32 @@
 #![no_std]
 
 extern crate alloc;
+
+extern crate contract_ffi;
+
 use alloc::vec::Vec;
 
-extern crate casperlabs_contract_ffi;
-use casperlabs_contract_ffi::contract_api::{call_contract, revert, get_uref};
-use casperlabs_contract_ffi::contract_api::pointers::ContractPointer;
-use casperlabs_contract_ffi::key::Key;
+use contract_ffi::contract_api::pointers::ContractPointer;
+use contract_ffi::contract_api::{self, Error};
+use contract_ffi::key::Key;
+use contract_ffi::unwrap_or_revert::UnwrapOrRevert;
 
 #[no_mangle]
 pub extern "C" fn call() {
-    let counter_uref = get_uref("counter").unwrap_or_else(|| revert(100));
+    let counter_uref = contract_api::get_key("counter").unwrap_or_revert_with(Error::User(100));
     let pointer = if let Key::Hash(hash) = counter_uref {
         ContractPointer::Hash(hash)
     } else {
-        revert(66)
+        contract_api::revert(Error::User(66))
     };
 
-    let _result: () = {
+    {
         let arg = "inc";
-        call_contract(pointer.clone(), &(arg,), &Vec::new())
-    };
+        contract_api::call_contract::<_, ()>(pointer.clone(), &(arg,), &Vec::new())
+    }
 
-    let _value: i32 = {
+    let _result: i32 = {
         let arg = "get";
-        call_contract(pointer, &(arg,), &Vec::new())
+        contract_api::call_contract(pointer, &(arg,), &Vec::new())
     };
 }
--- contracts/counter-define/src/lib.rs
+++ contracts/counter-define/src/lib.rs
@@ -1,24 +1,31 @@
 #![no_std]
 
 extern crate alloc;
+
+extern crate contract_ffi;
+
 use alloc::collections::BTreeMap;
 use alloc::string::String;
 use alloc::vec::Vec;
 
-extern crate casperlabs_contract_ffi;
-use casperlabs_contract_ffi::contract_api::*;
-use casperlabs_contract_ffi::contract_api::pointers::UPointer;
-use casperlabs_contract_ffi::key::Key;
+use contract_ffi::contract_api::pointers::TURef;
+use contract_ffi::contract_api::{self, Error};
+use contract_ffi::key::Key;
+use contract_ffi::unwrap_or_revert::UnwrapOrRevert;
 
 #[no_mangle]
 pub extern "C" fn counter_ext() {
-    let i_key: UPointer<i32> = get_uref("count").unwrap().to_u_ptr().unwrap();
-    let method_name: String = get_arg(0);
+    let turef: TURef<i32> = contract_api::get_key("count").unwrap().to_turef().unwrap();
+    let method_name: String = contract_api::get_arg(0)
+        .unwrap_or_revert_with(Error::MissingArgument)
+        .unwrap_or_revert_with(Error::InvalidArgument);
     match method_name.as_str() {
-        "inc" => add(i_key, 1),
+        "inc" => contract_api::add(turef, 1),
         "get" => {
-            let result = read(i_key);
-            ret(&result, &Vec::new());
+            let result = contract_api::read(turef)
+                .unwrap_or_revert_with(Error::Read)
+                .unwrap_or_revert_with(Error::ValueNotFound);
+            contract_api::ret(&result, &Vec::new());
         }
         _ => panic!("Unknown method name!"),
     }
@@ -26,13 +33,13 @@ pub extern "C" fn counter_ext() {
 
 #[no_mangle]
 pub extern "C" fn call() {
-    let counter_local_key = new_uref(0); //initialize counter
+    let counter_local_key = contract_api::new_turef(0); //initialize counter
 
     //create map of references for stored contract
     let mut counter_urefs: BTreeMap<String, Key> = BTreeMap::new();
     let key_name = String::from("count");
     counter_urefs.insert(key_name, counter_local_key.into());
 
-    let pointer = store_function("counter_ext", counter_urefs);
-    add_uref("counter", &pointer.into());
+    let pointer = contract_api::store_function_at_hash("counter_ext", counter_urefs);
+    contract_api::put_key("counter", &pointer.into());
 }
