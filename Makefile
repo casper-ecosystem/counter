a7a807655bcd43f70b7d92d337b37fbef24ed7c7
Maciej Zielinski <maciek.s.zielinski@gmail.com>
2021-04-19 12:57:39 +0200

    Update to 1.0.1 crates

--- Makefile
+++ Makefile
@@ -9,14 +9,4 @@ lint:
 	cargo clippy --all-targets --all -- -D warnings -A renamed_and_removed_lints
 
 build-contracts:
-	rm -f target/wasm32-unknown-unknown/release/*wasm
 	cargo build --release --target wasm32-unknown-unknown
-
-deploy: build-contracts
-	cd scripts && ./deploy-smart-contract.sh
-
-increment: build-contracts
-	cd scripts && ./increment-counter.sh
-
-check-counter:
-	cd scripts && ./check-counter.sh
--- contracts/counter-define/src/main.rs
+++ contracts/counter-define/src/main.rs
@@ -39,12 +39,12 @@ pub extern "C" fn counter_get() {
 pub extern "C" fn call() {
     let counter_local_key = storage::new_uref(0); //initialize counter
 
-    //create map of references for stored contract
+    // Create initial named keys of the contract.
     let mut counter_named_keys: BTreeMap<String, Key> = BTreeMap::new();
     let key_name = String::from(COUNT_KEY);
     counter_named_keys.insert(key_name, counter_local_key.into());
 
-    // create entry point
+    // Create entry point
     let mut counter_entry_points = EntryPoints::new();
     counter_entry_points.add_entry_point(EntryPoint::new(
         COUNTER_INC,
