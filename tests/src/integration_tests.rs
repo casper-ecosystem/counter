d85405258c4504abe8ab2eba0678f70ca9a3e3e4
Hans <hans@casperlabs.io>
2021-07-21 07:19:27 -0600

    fix in integration_tests

--- tests/src/integration_tests.rs
+++ tests/src/integration_tests.rs
@@ -16,7 +16,7 @@ mod tests {
 
     impl CounterContract {
         pub fn deploy() -> Self {
-            let public_key: PublicKey = SecretKey::ed25519_from_bytes([1u8; 32]).unwrap().into();
+            let public_key = PublicKey::from(&SecretKey::ed25519_from_bytes([1u8; 32]).unwrap());
             let account_addr = AccountHash::from(&public_key);
             let mut context = TestContextBuilder::new()
                 .with_public_key(public_key, U512::from(100_000_000_000_000u64))
