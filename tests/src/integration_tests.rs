2a9821ec78714429a27ff2075e95d0b550ffa5e6
ipopescu <ipopescu@users.noreply.github.com>
2022-09-22 20:35:22 +0200

    Fix linting error

--- tests/src/integration_tests.rs
+++ tests/src/integration_tests.rs
@@ -19,7 +19,7 @@ mod tests {
         let mut builder = InMemoryWasmTestBuilder::default();
         builder.run_genesis(&*DEFAULT_RUN_GENESIS_REQUEST).commit();
 
-        let contract_installation_request = ExecuteRequestBuilder::standard( 
+        let contract_installation_request = ExecuteRequestBuilder::standard(
             *DEFAULT_ACCOUNT_ADDR,
             COUNTER_DEFINE_WASM,
             runtime_args! {},
