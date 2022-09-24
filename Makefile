56cd5be0e1e2b6ff9a1ac8a947fd73632d263729
ipopescu <ipopescu@users.noreply.github.com>
2022-09-24 12:21:55 +0200

    New repository structure

--- Makefile
+++ Makefile
@@ -2,37 +2,46 @@ prepare:
 	rustup target add wasm32-unknown-unknown
 
 build-contract:
-	cd contract && cargo build --release --target wasm32-unknown-unknown
+	cd contract-v1 && cargo build --release --target wasm32-unknown-unknown
+	cd contract-v2 && cargo build --release --target wasm32-unknown-unknown
 	cd counter-call && cargo build --release --target wasm32-unknown-unknown
 
-	wasm-strip contract/target/wasm32-unknown-unknown/release/contract.wasm 2>/dev/null | true
-	wasm-strip counter-call/target/wasm32-unknown-unknown/release/contract.wasm 2>/dev/null | true
+	wasm-strip contract-v1/target/wasm32-unknown-unknown/release/contract-v1.wasm 2>/dev/null | true
+	wasm-strip counter-call/target/wasm32-unknown-unknown/release/contract-v1.wasm 2>/dev/null | true
+
+	wasm-strip contract-v2/target/wasm32-unknown-unknown/release/contract-v2.wasm 2>/dev/null | true
+	wasm-strip counter-call/target/wasm32-unknown-unknown/release/contract-v2.wasm 2>/dev/null | true
 
 test-only:
 	cd tests && cargo test
 
 test: build-contract
 	mkdir -p tests/wasm
-	cp contract/target/wasm32-unknown-unknown/release/counter-define.wasm tests/wasm
+	cp contract-v1/target/wasm32-unknown-unknown/release/counter-define.wasm tests/wasm
+	cp contract-v2/target/wasm32-unknown-unknown/release/counter-define.wasm tests/wasm
 	cp counter-call/target/wasm32-unknown-unknown/release/counter-call.wasm tests/wasm
 	cd tests && cargo test
 	
 clippy:
-	cd contract && cargo clippy --all-targets -- -D warnings
+	cd contract-v1 && cargo clippy --all-targets -- -D warnings
+	cd contract-v2 && cargo clippy --all-targets -- -D warnings
 	cd tests && cargo clippy --all-targets -- -D warnings
 
 check-lint: clippy
-	cd contract && cargo fmt -- --check
+	cd contract-v1 && cargo fmt -- --check
+	cd contract-v2 && cargo fmt -- --check
 	cd counter-call && cargo fmt -- --check
 	cd tests && cargo fmt -- --check
 
 lint: clippy
-	cd contract && cargo fmt
+	cd contract-v1 && cargo fmt
+	cd contract-v2 && cargo fmt
 	cd counter-call && cargo fmt
 	cd tests && cargo fmt
 
 clean:
-	cd contract && cargo clean
+	cd contract-v1 && cargo clean
+	cd contract-v2 && cargo clean
 	cd counter-call && cargo clean
 	cd tests && cargo clean
 	rm -rf tests/wasm
---
+++ contract-v2/src/main.rs
@@ -0,0 +1,99 @@
+#![no_std]
+#![no_main]
+
+#[cfg(not(target_arch = "wasm32"))]
+compile_error!("target arch should be wasm32: compile with '--target wasm32-unknown-unknown'");
+
+extern crate alloc;
+
+use alloc::{
+    collections::BTreeMap,
+    string::{String, ToString},
+    vec::Vec,
+};
+use casper_contract::{
+    contract_api::{runtime, storage},
+    unwrap_or_revert::UnwrapOrRevert,
+};
+use casper_types::{
+    api_error::ApiError,
+    contracts::{EntryPoint, EntryPointAccess, EntryPointType, EntryPoints},
+    CLType, CLValue, Key, URef,
+};
+
+const ENTRY_POINT_COUNTER_INC: &str = "counter_inc";
+const ENTRY_POINT_COUNTER_GET: &str = "counter_get";
+
+const CONTRACT_VERSION_KEY: &str = "version";
+const COUNT_KEY: &str = "count";
+
+#[no_mangle]
+pub extern "C" fn counter_inc() {
+    let uref: URef = runtime::get_key(COUNT_KEY)
+        .unwrap_or_revert_with(ApiError::MissingKey)
+        .into_uref()
+        .unwrap_or_revert_with(ApiError::UnexpectedKeyVariant);
+    storage::add(uref, 1); // increment the count by 1
+}
+
+#[no_mangle]
+pub extern "C" fn counter_get() {
+    let uref: URef = runtime::get_key(COUNT_KEY)
+        .unwrap_or_revert_with(ApiError::MissingKey)
+        .into_uref()
+        .unwrap_or_revert_with(ApiError::UnexpectedKeyVariant);
+    let result: i32 = storage::read(uref)
+        .unwrap_or_revert_with(ApiError::Read)
+        .unwrap_or_revert_with(ApiError::ValueNotFound);
+    let typed_result = CLValue::from_t(result).unwrap_or_revert();
+    runtime::ret(typed_result); // return the count value
+}
+
+#[no_mangle]
+pub extern "C" fn call() {
+    // Initialize the count to 0 locally
+    let count_start = storage::new_uref(0_i32);
+
+    // In the named keys of the contract, add a key for the count
+    let mut counter_named_keys: BTreeMap<String, Key> = BTreeMap::new();
+    let key_name = String::from(COUNT_KEY);
+    counter_named_keys.insert(key_name, count_start.into());
+
+    // Create entry points for this contract
+    let mut counter_entry_points = EntryPoints::new();
+
+    counter_entry_points.add_entry_point(EntryPoint::new(
+        ENTRY_POINT_COUNTER_GET,
+        Vec::new(),
+        CLType::I32,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    ));
+
+    counter_entry_points.add_entry_point(EntryPoint::new(
+        ENTRY_POINT_COUNTER_INC,
+        Vec::new(),
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    ));
+
+    // Create a new contract package that can be upgraded
+    let (stored_contract_hash, contract_version) = storage::new_contract(
+        counter_entry_points,
+        Some(counter_named_keys),
+        Some("counter_package_name".to_string()),
+        Some("counter_access_uref".to_string()),
+    );
+
+    /* To create a locked contract instead, use new_locked_contract and throw away the contract version returned
+    let (stored_contract_hash, _) =
+        storage::new_locked_contract(counter_entry_points, Some(counter_named_keys), None, None); */
+
+    // Store the contract version in the context's named keys
+    let version_uref = storage::new_uref(contract_version);
+    runtime::put_key(CONTRACT_VERSION_KEY, version_uref.into());
+
+    // Create a named key for the contract hash
+    runtime::put_key("counter", stored_contract_hash.into());
+}
